04d57daef2f5e8bb260165ffdb5ba6cfae032b33
Johny Herangi <johnyherangi@gmail.com>
2022-09-22 21:53:22 +1200

    tidy up

--- src/password.ts
+++ src/password.ts
@@ -1,12 +1,12 @@
-export default function generate(length: number = 32) {
-    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    var lowercase = "abcdefghijklmnopqrstuvwxyz"
-    var numbers = "0123456789"
-    var symbols = "!\"#$%&'()*+,-./:;<=>?@^[\\]^_`{|}~"
-    var all = uppercase + lowercase + numbers + symbols
-    var password = ""
-    for (var index = 0; index < length; index++) {
-        var character = Math.floor(Math.random() * all.length)
+export default function generate(length = 32): string {
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const lowercase = "abcdefghijklmnopqrstuvwxyz"
+    const numbers = "0123456789"
+    const symbols = "!\"#$%&'()*+,-./:;<=>?@^[\\]^_`{|}~"
+    const all = uppercase + lowercase + numbers + symbols
+    let password = ""
+    for (let index = 0; index < length; index++) {
+        const character = Math.floor(Math.random() * all.length)
         password += all.substring(character, character + 1)
     }
     return password
